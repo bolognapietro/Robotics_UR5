d618a74c7897877315a09109d56b22ef7deb7e8e
luckeez <pluca1609@gmail.com>
2023-06-23 10:59:06 +0200

    new pkg name

--- pietro/cpp/cmake-build-debug/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/pietro/ros_ws/src/locosim/robot_control/lab_exercises/lab_palopoli/cpp/cmake-build-debug/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
